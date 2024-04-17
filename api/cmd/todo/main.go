0d9000fe40051476a0c3395a53380e4ee2630f28
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2024-04-17 11:28:41 -0300

    feat(api): :sparkles: creating features, learning about the cli and file storage

--- api/cmd/todo/main.go
+++ api/cmd/todo/main.go
@@ -3,29 +3,85 @@ package main
 import (
 	"api/pkg/tasks"
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
-	// Criar algumas tarefas de exemplo
-	task1 := tasks.Task{
-		Title:       "Fazer compras",
-		Description: "Comprar leite e pão",
-		CreatedAt:   time.Now(),
+	taskList := tasks.TaskList{}
+
+	// Carregar tarefas do arquivo (se existir)
+	err := taskList.CarregarFromFile("tasks.json")
+	if err != nil {
+		fmt.Println("Erro ao carregar tarefas:", err)
 	}
 
-	task2 := tasks.Task{
-		Title:       "Estudar Go",
-		Description: "Ler o livro de Go",
-		CreatedAt:   time.Now(),
-	}
+	defer func() {
+		// Salvar tarefas no arquivo ao sair
+		err := taskList.SalvarToFile("tasks.json")
+		if err != nil {
+			fmt.Println("Erro ao salvar tarefas:", err)
+		}
+	}()
 
-	// Adicionar as tarefas a uma lista de tarefas
-	taskList := tasks.TaskList{Tasks: []tasks.Task{task1, task2}}
+	// Exibir opções
+	fmt.Println("O que você deseja fazer?")
+	fmt.Println("1. Adicionar uma tarefa")
+	fmt.Println("2. Atualizar uma tarefa")
+	fmt.Println("3. Excluir uma tarefa")
+	fmt.Println("4. Listar todas as tarefas")
+	fmt.Println("5. Sair")
 
-	// Exibir as tarefas
-	fmt.Println("Tarefas:")
-	for _, task := range taskList.Tasks {
-		fmt.Printf("- %s\n", task.Title)
+	var opcao int
+	fmt.Scanln(&opcao)
+
+	switch opcao {
+	case 1:
+		var title, description string
+		fmt.Println("Digite o título da tarefa:")
+		fmt.Scanln(&title)
+		fmt.Println("Digite a descrição da tarefa:")
+		fmt.Scanln(&description)
+		taskList.AdicionarTask(title, description)
+		fmt.Println("Tarefa adicionada com sucesso!")
+	case 2:
+		var title, newTitle, newDescription string
+		fmt.Println("Digite o título da tarefa que deseja atualizar:")
+		fmt.Scanln(&title)
+		fmt.Println("Digite o novo título da tarefa:")
+		fmt.Scanln(&newTitle)
+		fmt.Println("Digite a nova descrição da tarefa:")
+		fmt.Scanln(&newDescription)
+		err := taskList.AtualizarTask(title, newTitle, newDescription)
+		if err != nil {
+			fmt.Println("Erro:", err)
+		} else {
+			fmt.Println("Tarefa atualizada com sucesso!")
+		}
+	case 3:
+		var title string
+		fmt.Println("Digite o título da tarefa que deseja excluir:")
+		fmt.Scanln(&title)
+		err := taskList.ExcluirTask(title)
+		if err != nil {
+			fmt.Println("Erro:", err)
+		} else {
+			fmt.Println("Tarefa excluída com sucesso!")
+		}
+	case 4:
+		fmt.Println("Tarefas:")
+		for _, task := range taskList.Tasks {
+			fmt.Printf("- %s\n", task.Title)
+			fmt.Printf("  Descrição: %s\n", task.Description)
+			fmt.Printf("  Criada em: %s\n", task.CreatedAt.Format(time.RFC3339))
+			if !task.CompletedAt.IsZero() {
+				fmt.Printf("  Concluída em: %s\n", task.CompletedAt.Format(time.RFC3339))
+			}
+		}
+	case 5:
+		fmt.Println("Saindo...")
+		os.Exit(0)
+	default:
+		fmt.Println("Opção inválida")
 	}
 }
--- api/pkg/tasks/tasks.go
+++ api/pkg/tasks/tasks.go
@@ -1,14 +1,70 @@
 package tasks
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
 
 type Task struct {
-    Title       string
-    Description string
-    CreatedAt   time.Time
-    CompletedAt time.Time
+	Title       string
+	Description string
+	CreatedAt   time.Time
+	CompletedAt time.Time
 }
 
 type TaskList struct {
-    Tasks []Task
+	Tasks []Task
+}
+
+// AdicionarTask adiciona uma nova tarefa à lista de tarefas
+func (tl *TaskList) AdicionarTask(title, description string) {
+	task := Task{
+		Title:       title,
+		Description: description,
+		CreatedAt:   time.Now(),
+	}
+	tl.Tasks = append(tl.Tasks, task)
+}
+
+// AtualizarTask atualiza uma tarefa existente na lista de tarefas
+func (tl *TaskList) AtualizarTask(title, newTitle, newDescription string) error {
+	for i, task := range tl.Tasks {
+		if task.Title == title {
+			tl.Tasks[i].Title = newTitle
+			tl.Tasks[i].Description = newDescription
+			return nil
+		}
+	}
+	return fmt.Errorf("tarefa '%s' não encontrada", title)
+}
+
+// ExcluirTask exclui uma tarefa da lista de tarefas
+func (tl *TaskList) ExcluirTask(title string) error {
+	for i, task := range tl.Tasks {
+		if task.Title == title {
+			tl.Tasks = append(tl.Tasks[:i], tl.Tasks[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("tarefa '%s' não encontrada", title)
+}
+
+// SalvarToFile salva a lista de tarefas em um arquivo JSON
+func (tl *TaskList) SalvarToFile(filename string) error {
+	data, err := json.Marshal(tl)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+// CarregarFromFile carrega a lista de tarefas de um arquivo JSON
+func (tl *TaskList) CarregarFromFile(filename string) error {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, tl)
 }
